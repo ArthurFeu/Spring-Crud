f09a5caefe25bf54c727af88b0766a4d1a697f2c
Arthur Feu <arthurfeufeu@gmail.com>
2024-09-10 16:48:48 -0300

    Initial commit, basic setup.

---
+++ src/main/java/com/arthur/spring_crud/SpringCrudApplication.java
@@ -0,0 +1,14 @@
+package com.arthur.spring_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude =  {DataSourceAutoConfiguration.class })
+public class SpringCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudApplication.class, args);
+	}
+
+}
